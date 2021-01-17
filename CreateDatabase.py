260b1ae612ba82d6dfc8361536bfd0e6069a4c9a
DanielPetrache <54598299+DanielPetrache@users.noreply.github.com>
2021-01-17 19:55:46 +0200

    Add files via upload

---
+++ CreateDatabase.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+conn = sqlite3.connect('emojis.db')
+
+c = conn.cursor()
+
+print(c.execute("PRAGMA foreign_keys;"))
+
+tables_creation = ['''CREATE TABLE IF NOT EXISTS users(
+                  id TEXT PRIMARY KEY NOT NULL ,
+                  user_name TEXT NOT NULL
+                  );''',
+                  
+                  '''CREATE TABLE IF NOT EXISTS emojis(
+                  id TEXT PRIMARY KEY NOT NULL,
+                  emoji_name TEXT NOT NULL
+                  );''',
+                  
+                  '''CREATE TABLE IF NOT EXISTS emoji_count(
+                  user_id TEXT NOT NULL,
+                  emoji_id TEXT NOT NULL,
+                  user_name TEXT NOT NULL,
+                  emoji_name TEXT NOT NULL,
+                  counter INTEGER NOT NULL,
+                  FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+                  FOREIGN KEY(emoji_id) REFERENCES emojis(id) ON DELETE CASCADE
+                  PRIMARY KEY (user_id, emoji_id)
+                  );''']
+                  
+
+                  
+for x in tables_creation:
+    print(x)
+    c.execute(x)
+
+conn.commit()
+conn.close()
